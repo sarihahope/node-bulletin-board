33d988b1d5eb83de514efcefd121c94649923b98
sarihahope <sarihaslink@yahoo.com>
2024-10-02 18:13:08 -0700

    som thing

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// For every .js file in the src directory, require at least 80% line coverage.
-module.exports = {
-    coverageThreshold: {
-      './src/*.js': {
-        lines: 80
-      }
-    }
-  }
